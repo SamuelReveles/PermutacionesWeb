405665f99198508120fe4595d72e681be13d171d
SamuelReveles <sabyreveles@gmail.com>
2023-10-22 22:36:24 -0600

    Funciones básicas

---
+++ app.js
@@ -0,0 +1,43 @@
+function remCharAt(str, pos){
+    let arr = str.split('');
+    arr.splice(pos, 1);
+    return arr.join('');
+}
+
+function permutaciones(palabra){
+    if(palabra.length == 1) return [palabra];
+  
+    let perm, res = [];
+  
+    for(let i = 0; i < palabra.length; i++){
+      perm = permutaciones(remCharAt(palabra,i));
+      perm = perm.map(function(v){return this+v}, palabra[i]);
+      res = res.concat(perm);
+    }
+    
+    return (res);
+}
+
+function factorial(num) {
+    resultado = num;
+    for(i = 1; i <= num; i++) {resultado *= i;}
+    return resultado;
+}
+
+function arrToString(palabra) {
+    res = "Hay " + factorial(palabra.length) + " permutaciones\n";
+    array = permutaciones(palabra);
+    array.forEach(element => {
+        res += (element + ', ');
+    });
+    return res;
+}
+
+function mostrarPermutaciones() {
+    const palabra = document.getElementById('palabra');
+    document.getElementById("respuesta").value = arrToString(palabra.value);
+    // console.log(permutaciones(palabra.value));
+}
+
+const boton = document.getElementById("boton");
+boton.onclick = mostrarPermutaciones;
